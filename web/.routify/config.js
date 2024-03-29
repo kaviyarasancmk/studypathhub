f9db65ce15394443b4be14410522f5cac220730d
kaviyarasancmk <cmkkaviyarasanmm@gmail.com>
2024-03-29 23:00:44 +0530

    Add new them

---
+++ web/.routify/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "pages": "src/pages",
+  "sourceDir": "public",
+  "routifyDir": ".routify",
+  "ignore": "",
+  "dynamicImports": true,
+  "singleBuild": false,
+  "noHashScroll": false,
+  "distDir": "dist",
+  "hashScroll": true,
+  "extensions": [
+    "html",
+    "svelte",
+    "md",
+    "svx"
+  ],
+  "started": "2024-03-29T16:07:56.233Z"
+}
